430de16c4569af23bc131e75d1d2b9beb4010679
Corey <corey@thedevenports.com>
2021-09-16 14:56:22 -0700

    better random strings

--- hash.py
+++ hash.py
@@ -1,6 +1,6 @@
 import hashlib
 import random
-from string import ascii_letters
+from string import ascii_letters, ascii_uppercase, digits
 
 h_str = input("String to hash: ")
 n = input("Number of bits to test: ")
@@ -10,7 +10,7 @@ i = 0
 h_list.append(hashlib.sha1(bytes(h_str.encode())).digest()[0:int(n)])
 
 while True:
-    h = hashlib.sha1(bytes(''.join(random.choice(ascii_letters) for j in range(len(h_str))).encode())).digest()[0:int(n)]
+    h = hashlib.sha1(bytes(''.join(random.choices(ascii_letters + digits, k=len(h_str))).encode())).digest()[0:int(n)]
     if h in h_list:
         print("Collision at {}".format(i))
         break
