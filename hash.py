14659625c9b017eb5e969596d6b23a470dbebe3a
Corey <corey@thedevenports.com>
2021-09-15 19:17:52 -0700

    initial commit

--- hash.py
+++ hash.py
@@ -1,1 +1,17 @@
+import hashlib
+import random
+
+
+n = input("Number of bits to test: ")
+h_list = list()
+i = 0
+
+while True:
+    h = hashlib.sha1(bytearray(random.getrandbits(n))).hexdigest()[0:n]
+    if h in h_list:
+        print("Collision at %d".format(i))
+        break
+    else:
+        h_list.append(h)
+        i += 1
 
