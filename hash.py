1ceb1bd8f05b7add2be92f85bae013d4a7cb4345
Corey <corey@thedevenports.com>
2021-09-16 20:06:45 -0700

    added pre-image attack

--- hash.py
+++ hash.py
@@ -5,6 +5,7 @@ from string import ascii_letters, ascii_uppercase, digits
 
 h_str = input("String to hash: ")
 n = input("Number of bits to test: ")
+is_collision = input("Perform collision or pre-image attack? (c/p): ")
 h_list = list()
 i = 0
 
@@ -17,15 +18,30 @@ h_start = int.from_bytes(h_start, byteorder='big') >> remain
 
 rando = 'bananas' + str(random.randint(0, 100))
 
-while True:
-    rando = rando + str(i)
-    h = hashlib.sha1(rando.encode()).digest()[:ceil(n/8)]
-    h = int.from_bytes(h, byteorder='big') >> remain
-    if h == h_start:
-        print("Collision at {}".format(i))
-        break
-    else:
-        #print(h)
-        #h_list.append(h)
-        i += 1
+
+if (is_collision == 'c'):
+    while True:
+        rando = rando + str(i)
+        h = hashlib.sha1(rando.encode()).digest()[:ceil(n/8)]
+        h = int.from_bytes(h, byteorder='big') >> remain
+        if h == h_start:
+            print("Collision at {}".format(i))
+            break
+        else:
+            #print(h)
+            #h_list.append(h)
+            i += 1
+else:
+    h_list.append(h_start)
+    while True:
+        rando = rando + str(i)
+        h = hashlib.sha1(rando.encode()).digest()[:ceil(n/8)]
+        h = int.from_bytes(h, byteorder='big') >> remain
+        if h in h_list:
+            print("Collision at {}".format(i))
+            break
+        else:
+            #print(h)
+            h_list.append(h)
+            i += 1
 
