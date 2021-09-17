69b463604d089034cb91775e3f6ee264c6a26db9
Corey <corey@thedevenports.com>
2021-09-16 18:38:26 -0700

    change rando

--- hash.py
+++ hash.py
@@ -10,11 +10,11 @@ i = 0
 h_start = hashlib.sha1(h_str.encode()).digest()[0:int(n)]
 #print(h_start)
 
-rando = ''.join(random.choices(ascii_letters + digits, k=len(h_str))).encode()
+rando = 'bananas'
 
 while True:
-    rando = rando + bytes(i)
-    h = hashlib.sha1(rando).digest()[0:int(n)]
+    rando = rando + str(i)
+    h = hashlib.sha1(rando.encode()).digest()[0:int(n)]
     if h == h_start:
         print("Collision at {}".format(i))
         break
